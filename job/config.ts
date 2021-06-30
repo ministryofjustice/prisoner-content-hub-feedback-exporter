7913d110b6a59200b0b3d2520bd520098933e289
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-06-30 21:55:46 +0100

    Deploying to staging and production

--- job/config.ts
+++ job/config.ts
@@ -1,13 +1,17 @@
 import 'dotenv/config'
 
-const elasticsearchEndpoint = process.env.ELASTICSEARCH_ENDPOINT || 'http://localhost:9200'
+const {
+  ELASTICSEARCH_ENDPOINT: elasticsearchEndpoint = 'http://localhost:9200',
+  SERVICE_ACCOUNT_KEY: serviceAccountKey,
+  SPREADSHEET_ID: spreadsheetId,
+} = process.env
 
 export = {
   elasticsearch: {
     feedback: `${elasticsearchEndpoint}/prod-feedback/_search`,
   },
   sheetsClient: {
-    serviceAccountKey: process.env.SERVICE_ACCOUNT_KEY,
-    spreadsheetId: process.env.SPREADSHEET_ID,
+    serviceAccountKey,
+    spreadsheetId,
   },
 }
--- job/feedbackRetriever.ts
+++ job/feedbackRetriever.ts
@@ -3,6 +3,7 @@ import config from './config'
 import type HttpClient from './utils/httpClient'
 
 const columnNames = ['date', 'title', 'contentType', 'sentiment', 'comment', 'sessionId', 'establishment', 'series']
+
 function toLine(result: any): string[] {
   // eslint-disable-next-line no-underscore-dangle
   return columnNames.map(columnName => result._source[columnName])
